8ee750a511fab38f02a32ac707490fb7d54a8b6c
ankit-dataniya <mk561995@gmail.com>
2026-08-12 13:21:31 +0000

    Ankit daily update

--- read_daily_docs.php
+++ read_daily_docs.php
@@ -1,1 +1,5 @@
 <?php
+// 2026-08-12 13:21:26
+/*
+To prevent mass assignment vulnerabilities in Laravel, you can use the `$fillable` property in your model to specify which attributes can be mass-assigned when creating or updating a model instance.
+*/
